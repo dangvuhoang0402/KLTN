6aefdb1ac766b0da030b881aa077e405dccd633c
dangvuhoang0402 <dangvuhoang0402@gmail.com>
2025-06-24 13:20:06 +0700

    commit

--- src/config/database.config.js
+++ src/config/database.config.js
@@ -2,7 +2,7 @@ const mongoose = require('mongoose');
 
 const connectMongoDB = (connectString) => {
     return mongoose.connect(connectString, {
-        serverSelectionTimeoutMS: 60000 // 60 seconds timeout
+        serverSelectionTimeoutMS: 100000 // 60 seconds timeout
     })
     .then(() => console.log("Connected to DB"))
     .catch(err => console.error("MongoDB connection error:", err));
