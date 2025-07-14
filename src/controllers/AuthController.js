e9234cf0dde9ce853c58e8380dd75dfbb640edee
dangvuhoang0402 <dangvuhoang0402@gmail.com>
2025-07-14 23:04:48 +0700

    commit

--- src/controllers/AuthController.js
+++ src/controllers/AuthController.js
@@ -1,15 +1,21 @@
 const authService = require("../services/AuthService")
 
 const login = async (req, res, next) => {
-    const result = await authService.login(req.body);
-    if (result && result.token) {
-        res.cookie('jwt', result.token, {
-            httpOnly: true,
-            secure: process.env.NODE_ENV === 'production'
-        });
-        return res.redirect('/home');
+    try {
+        const result = await authService.login(req.body);
+        if (result && result.token) {
+            res.cookie('jwt', result.token, {
+                httpOnly: true,
+                secure: process.env.NODE_ENV === 'production'
+            });
+            return res.redirect('/home');
+        }
+        // This should not be reached, but just in case:
+        return res.render('pages/login_fail', { layout: false });
+    } catch (error) {
+        // On login fail, render the fail page
+        return res.render('pages/login_fail', { layout: false, error: error.message });
     }
-    return res.redirect('/auth/login',);
 };
 
 const logout = async (req, res, next) => {
